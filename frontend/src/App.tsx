b891e95396ad5528e91748018f4ba5cf48ab729d
Malinda <salgaduwml@gmail.com>
2024-05-22 13:41:40 +0530

    create react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div>
+      <Button>Bs Button</Button>
+    </div>
+  );
+}
+
+export default App;
